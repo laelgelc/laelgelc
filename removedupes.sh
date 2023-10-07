f862ac24c53e0f3056f485a7c757392df35a96fb
eyamrog <eyamrog@gmail.com>
2023-10-07 16:45:27 -0300

    Add files via upload

--- removedupes.sh
+++ removedupes.sh
@@ -5,6 +5,7 @@ clear
 cut -d'|' -f3 images/images_index.txt | sed 's/id://' | sort | uniq -c | grep -v ' 1 ' | nl | sed 's/^[ ]*//' | tr '\t' ' ' | tr -s ' ' > d
 
 rm -f remove
+
 while read n maxhits id
 do
     echo "--- listing $n ---"
@@ -17,14 +18,16 @@ do
 done < d 
 
 # remove duplicate image files
-while read dupe
-do
-    pretty=$( echo $dupe | sed 's/t://' )
-    folder=$( grep $dupe images/images_index.txt | cut -d'|' -f1 | sed 's/fl://' )
-    ext=$( grep $dupe images/images_index.txt | cut -d'|' -f7 | sed 's/f://' )
-    rm -f images/images/"$folder"/"$pretty"."$ext"
-    echo "--- removing images/images/"$folder"/"$pretty"."$ext" ---"
-done < remove
+if [[ -s remove ]]; then
+    while read dupe
+    do
+        pretty=$( echo $dupe | sed 's/t://' )
+        folder=$( grep $dupe images/images_index.txt | cut -d'|' -f1 | sed 's/fl://' )
+        ext=$( grep $dupe images/images_index.txt | cut -d'|' -f7 | sed 's/f://' )
+        rm -f images/images/"$folder"/"$pretty"."$ext"
+        echo "--- removing images/images/"$folder"/"$pretty"."$ext" ---"
+    done < remove
+fi
 
 # remove dupes from index
 #grep -vf remove images/images_index.txt > z ; mv z images/images_index.txt
@@ -35,14 +38,14 @@ fi
 # remove dupes from index again, for some reason some still remain
 cut -d'|' -f2  images/images_index.txt | cut -d':' -f2 > i
 find images/images -type f | cut -d'/' -f4 | cut -d'.' -f1 > f
-cat i i f | sort | uniq -c | grep ' 2 ' | cut -c6- | grep -v '^$' | sed 's/^/t:/' > ionly
+cat i i f | sort | uniq -c | grep ' 2 ' | cut -c9- | grep -v '^$' | sed 's/^/t:/' > ionly
 grep -vf ionly images/images_index.txt > b ; mv b images/images_index.txt
 
 # remove empty image files
-find images/images -type f empty -exec rm {} +
+find images/images -type f -empty -exec rm {} +
 
 # remove empty image files from index
 cut -d'|' -f2  images/images_index.txt | cut -d':' -f2 > i
 find images/images -type f | cut -d'/' -f4 | cut -d'.' -f1 > f
-cat i i f | sort | uniq -c | grep ' 2 ' | cut -c6- | grep -v '^$' | sed 's/^/t:/' > ionly
+cat i i f | sort | uniq -c | grep ' 2 ' | cut -c9- | grep -v '^$' | sed 's/^/t:/' > ionly
 grep -vf ionly images/images_index.txt > b ; mv b images/images_index.txt
--- toplabels.sh
+++ toplabels.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 clear
+cut -d'|' -f6 images/labels.txt | sed 's/l://' | tr ',' '\n' | sort | uniq -c | sort -nr | sed 's/^[ ]*//' | grep '[a-z]' | head -1000 | cut -d' ' -f2- | nl -nrz | sed 's/^/v/' | tr '\t' ' ' > images/selectedwords
 
+cp images/selectedwords images/var_index.txt
