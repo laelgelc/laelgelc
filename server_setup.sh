0e6d845f05b1d272308995b27c399a7d2ac62c93
Rogério Yamada <eyamrog@gmail.com>
2024-08-07 17:36:10 +0000

    20240807-1436

--- server_setup.sh
+++ server_setup.sh
@@ -7,7 +7,7 @@ sudo apt update && sudo apt upgrade -y || { echo "Failed to update and upgrade p
 
 # Installing necessary packages
 sudo snap install aws-cli --classic
-sudo apt install -y python3-pip python3-venv ripgrep html2text zip unzip pipx
+sudo apt install -y python3-pip python3-venv ripgrep html2text zip unzip pipx ffmpeg
 
 # Creating directory for TreeTagger
 mkdir -p "$HOME"/treetagger/
