f0bbd1c39155fc39003d35c527847e2b2221bebd
Rogério Yamada <eyamrog@gmail.com>
2025-07-17 12:40:12 -0300

    20250717-1240

---
+++ Jupyter_Notebook_Template.py
@@ -0,0 +1,74 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.17.2
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown]
+# <center>
+# <img src="https://laelgelcpublic.s3.sa-east-1.amazonaws.com/lael_50_years_narrow_white.png.no_years.400px_96dpi.png" width="300" alt="LAEL 50 years logo">
+# <h3>APPLIED LINGUISTICS GRADUATE PROGRAMME (LAEL)</h3>
+# </center>
+# <hr>
+
+# %% [markdown]
+# # Template
+
+# %% [markdown]
+# ## Dictionary template
+
+# %% [markdown]
+# ```
+# data = {'': [ '', '', '', ''],
+#         '': [ '', '', '', ''],
+#         '': [ '', '', '', ''],
+#         '': [ '', '', '', '']}
+# ```
+
+# %% [markdown]
+# ## Numpy array template
+
+# %% [markdown]
+# ```
+# array_a = np.array([[, , , , , ],
+#                     [, , , , , ],
+#                     [, , , , , ],
+#                     [, , , , , ],
+#                     [, , , , , ],
+#                     [, , , , , ]])
+# ```
+
+# %% [markdown]
+# ## What is `<>`?
+
+# %% [markdown]
+#
+
+# %% [markdown]
+# ## Required Python packages
+
+# %% [markdown]
+# - <>
+# - <>
+
+# %% [markdown]
+# ## Importing the required libraries
+
+# %%
+
+# %% [markdown]
+# ### Raw cell
+
+# %% [raw]
+# This is a raw cell
+
+# %%
