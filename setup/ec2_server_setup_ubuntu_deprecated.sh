d6168591a7d3e17ff858f11b2b972ec0cad26d4a
Rogério Yamada <eyamrog@gmail.com>
2026-08-03 19:19:28 -0300

    gitall_20260803-1919

--- setup/ec2_server_setup_ubuntu_deprecated.sh
+++ setup/ec2_server_setup_ubuntu_deprecated.sh
@@ -259,6 +259,58 @@ fi
 git config --global user.name "${USER}@$(hostname -s)"
 git config --global user.email "${USER}@$(hostname -s).local"
 
+# Write (don’t append) to keep this idempotent
+cat << 'EOF' > "$HOME/.gitignore_global"
+# General
+nohup.out
+
+# macOS system files
+.DS_Store
+.AppleDouble
+.LSOverride
+
+# Icon files
+Icon?
+
+# Thumbnails
+._*
+
+# macOS folder attributes
+.Spotlight-V100
+.Trashes
+
+# AFP share metadata
+.AppleDB
+.AppleDesktop
+Network Trash Folder
+Temporary Items
+.apdisk
+
+# RStudio project files
+.Rproj.user/
+*.Rproj
+
+# R session data
+.Rhistory
+.RData
+.Ruserdata
+
+# R package build artifacts
+.Rbuildignore
+.Rcheck/
+
+# R Markdown caches
+*_cache/
+cache/
+
+# renv
+renv/library/
+renv/staging/
+
+EOF
+
+git config --global core.excludesfile "$HOME/.gitignore_global"
+
 echo "--- Starting Firefox Non-Snap Setup ---"
 
 sudo snap remove firefox || true
